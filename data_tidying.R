2a251b6b8ab369b52a87f98dff5054a4ddcb6fb2
yeniuma <manczaladri@gmail.com>
2022-04-18 21:04:55 +0200

    reworked code

--- data_tidying.R
+++ data_tidying.R
@@ -3,28 +3,30 @@ library(academictwitteR)
 library(writexl)
 library(sentimentr)
 
+data_combining_func <- function(path, output_xlsx){
+  print("Combining JSONs into a data frame...")
+  all_tweets <- bind_tweets(data_path = path, output_format = "tidy")
+  print("Selecting columns...")
+  formatted_tweets <- all_tweets %>% select(tweet_id, text, created_at,user_location,
+                                            like_count, user_followers_count)
+  
+  n_of_rows <- nrow(formatted_tweets)
+  print("Getting sentiments...")
+  first_part_sentiments <- formatted_tweets[1:round(n_of_rows/2),2] %>%
+    get_sentences() %>% 
+    sentiment_by()
+  second_part_sentiments <- formatted_tweets[(round(n_of_rows/2)+1):n_of_rows,2] %>%
+    get_sentences() %>% 
+    sentiment_by()
+  print("Binding data frames together...")
+  all_sentiments <- bind_rows(first_part_sentiments,second_part_sentiments) %>% select(c(-element_id, -word_count))
+  print("Binding columns...")
+  output_df_name <- cbind(formatted_tweets, all_sentiments)
+  write_xlsx(output_df_name, output_xlsx)
+  print("Done!")
+  return(output_df_name)
+}
 
-all_tweets_retry <- bind_tweets(data_path = "twitter_adatok_btc_retry", output_format = "tidy")
-write_xlsx(all_tweets_retry,"all_btc_tweets_retry.xlsx")
+btc_tweets <- data_combining_func("twitter_adatok_btc_retry", "all_btc_tweets.xlsx")
+eth_tweets <- data_combining_func("twitter_adatok_eth", "all_eth_tweets.xlsx")
 
-formatted_tweets <- all_tweets_retry %>% select(tweet_id, text, created_at,user_location,
-                                                like_count, user_followers_count)
-
-first_part_tweets <- formatted_tweets[1:200000,]
-second_part_tweets <- formatted_tweets[200001:575511,]
-
-first_part_sentences <- get_sentences(first_part_tweets$text)
-second_part_sentences <- get_sentences(second_part_tweets$text)
-
-first_part_grouped_sentiments <- sentiment_by(first_part_sentences)
-second_part_grouped_sentiments <- sentiment_by(second_part_sentences)
-
-all_sentiments <- bind_rows(first_part_grouped_sentiments,second_part_grouped_sentiments) %>% select(c(-element_id, -word_count))
-
-btc_df_to_be_used <- cbind(formatted_tweets, all_sentiments)
-
-#sentences <- get_sentences(formatted_tweets$text)
-#sentiments <- sentiment(sentences)
-
-
-#grouped_sentiment <- sentiment_by(sentences)
