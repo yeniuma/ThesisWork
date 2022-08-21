1a3f6a84002c1b034397f6fa14a4be9cf039f6d6
yeniuma <manczaladri@gmail.com>
2022-08-21 17:45:44 +0200

    continued data analysis with Google Trends

--- google_trends_data_extr.R
+++ google_trends_data_extr.R
@@ -1,14 +1,18 @@
 library(gtrendsR)
 library(writexl)
-# install.packages("remotes")
-remotes::install_github("trendecon/trendecon")
 library(trendecon)
 
 btc_google_trends <- ts_gtrends(keyword = c("bitcoin","btc","Bitcoin", "BTC"), time = "2019-01-01 2022-03-30")
 
-btc_google_trends_daily <- ts_gtrends_mwd("bitcoin", from = "2019-01-01")
+btc_google_trends_daily <- ts_gtrends_mwd(keyword = c("bitcoin","btc","Bitcoin", "BTC"), from = "2019-01-01")
 
 btc_interest_over_time <- btc_google_trends$interest_over_time
 
 write.csv(btc_google_trends_daily, "btc_google_trends.csv")
+
+hist(btc_google_trends_daily$value)
+
+d <- density(btc_google_trends_daily$value)
+plot(d, type="n")
+polygon(d, col="red", border="gray")
 
